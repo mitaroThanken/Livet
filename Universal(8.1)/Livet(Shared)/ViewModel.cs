0bf58d7a96d641cfb31cd43b1135492a9cebcacd
YAMAMI Taro <mitarothanken@gmail.com>
2015-02-25 19:16:58 +0900

    .NET4.0からコピー。 DispatcherからCoreDispatcherに変更。 DataContractによるシリアライズに変更。 InvokeをRunAsyncに変更。 IsAssignableFromに関する変更。

---
+++ Universal(8.1)/Livet(Shared)/ViewModel.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Livet.Messaging;
+using System.Runtime.Serialization;
+
+namespace Livet
+{
+    /// <summary>
+    /// ViewModelの基底クラスです。
+    /// </summary>
+    [DataContract]
+    public abstract class ViewModel : NotificationObject,IDisposable
+    {
+        [IgnoreDataMember]
+        private bool _disposed;
+        [IgnoreDataMember]
+        private InteractionMessenger _messenger;
+        [IgnoreDataMember]
+        private LivetCompositeDisposable _compositeDisposable;
+
+        /// <summary>
+        /// このViewModelクラスの基本CompositeDisposableです。
+        /// </summary>
+        [DataMember]
+        public LivetCompositeDisposable CompositeDisposable
+        {
+            get
+            {
+                if (_compositeDisposable == null)
+                {
+                    _compositeDisposable = new LivetCompositeDisposable();
+                }
+                return _compositeDisposable;
+            }
+            set
+            {
+                _compositeDisposable = value;
+            }
+        }
+
+        /// <summary>
+        /// このViewModelクラスの基本Messegerインスタンスです。
+        /// </summary>
+        [DataMember]
+        public InteractionMessenger Messenger
+        {
+            get
+            {
+                if (_messenger == null)
+                {
+                    _messenger = new InteractionMessenger();
+                }
+                return _messenger;
+            }
+            set
+            {
+                _messenger = value;
+            }
+        }
+
+        /// <summary>
+        /// このインスタンスによって使用されているすべてのリソースを解放します。
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_disposed) return;
+            if (disposing)
+            {
+                if (_compositeDisposable != null)
+                {
+                    _compositeDisposable.Dispose();
+                }
+            }
+            _disposed = true;
+        }
+    }
+}
---
+++ Universal(8.1)/Livet(Shared)/ViewModelHelper.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Collections.ObjectModel;
+using System.Reflection;
+using Livet.EventListeners;
+using Windows.UI.Core;
+
+namespace Livet
+{
+    /// <summary>
+    /// ViewModelのヘルパークラスです。
+    /// </summary>
+    public static class ViewModelHelper
+    {
+        /// <summary>
+        /// 変更通知コレクションに連動する読み取り専用の変更通知コレクションを生成します。
+        /// </summary>
+        /// <typeparam name="TModel">ソース変更通知コレクションのアイテム型</typeparam>
+        /// <typeparam name="TViewModel">生成する連動コレクションのアイテム型</typeparam>
+        /// <param name="source">ソース変更通知コレクション</param>
+        /// <param name="converter">TModel型のインスタンスをTViewModel型のインスタンスに変換するFunc</param>
+        /// <param name="dispatcher">生成する連動コレクションが変更通知を行うDispatcher</param>
+        /// <returns>生成された連動コレクション</returns>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Reliability", "CA2000:DisposeObjectsBeforeLosingScope")]
+        public static ReadOnlyDispatcherCollection<TViewModel> CreateReadOnlyDispatcherCollection<TModel, TViewModel>(IList<TModel> source, Func<TModel, TViewModel> converter, CoreDispatcher dispatcher)
+        {
+            if (source == null) throw new ArgumentNullException("source");
+
+            var sourceAsNotifyCollection = source as INotifyCollectionChanged;
+            if (sourceAsNotifyCollection == null) throw new ArgumentException("sourceがINotifyCollectionChangedを実装していません");
+
+            var initCollection = new ObservableCollection<TViewModel>();
+
+            foreach (var model in source)
+            {
+                initCollection.Add(converter(model));
+            }
+
+            var target = new DispatcherCollection<TViewModel>(initCollection, dispatcher);
+            var result = new ReadOnlyDispatcherCollection<TViewModel>(target);
+
+            var collectionChangedListener = new CollectionChangedEventListener(sourceAsNotifyCollection);
+
+            result.EventListeners.Add(collectionChangedListener);
+
+            collectionChangedListener.RegisterHandler((sender, e) =>
+                {
+                    switch (e.Action)
+                    {
+                        case NotifyCollectionChangedAction.Add:
+                            var vm = converter((TModel) e.NewItems[0]);
+                            InvokeOnDispatcher(() => target.Insert(e.NewStartingIndex, vm),dispatcher);
+                            break;
+                        case NotifyCollectionChangedAction.Move:
+                            InvokeOnDispatcher(() => target.Move(e.OldStartingIndex, e.NewStartingIndex), dispatcher);
+                            break;
+                        case NotifyCollectionChangedAction.Remove:
+                            if (typeof (IDisposable).GetTypeInfo().IsAssignableFrom(typeof (TViewModel).GetTypeInfo()))
+                            {
+                                ((IDisposable) target[e.OldStartingIndex]).Dispose();
+                            }
+                            InvokeOnDispatcher(() => target.RemoveAt(e.OldStartingIndex), dispatcher);
+                            break;
+                        case NotifyCollectionChangedAction.Replace:
+                            if (typeof(IDisposable).GetTypeInfo().IsAssignableFrom(typeof(TViewModel).GetTypeInfo()))
+                            {
+                                ((IDisposable)target[e.NewStartingIndex]).Dispose();
+                            }
+                            var replaceVm = converter((TModel) e.NewItems[0]);
+                            InvokeOnDispatcher(() => target[e.NewStartingIndex] = replaceVm, dispatcher);
+                            break;
+                        case NotifyCollectionChangedAction.Reset:
+                            if (typeof(IDisposable).GetTypeInfo().IsAssignableFrom(typeof(TViewModel).GetTypeInfo()))
+                            {
+                                foreach (IDisposable item in target)
+                                {
+                                    item.Dispose();
+                                }
+                            }
+                            InvokeOnDispatcher(target.Clear, dispatcher);
+                            break;
+                        default:
+                            throw new ArgumentException();
+                    }
+                });
+
+            return result;
+        }
+
+        private static async void InvokeOnDispatcher(Action action, CoreDispatcher dispatcher)
+        {
+            await dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => action());
+        }
+    }
+}
