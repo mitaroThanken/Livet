64b127e74898b2084f1c74ecbff7abc1c6643738
YAMAMI Taro <mitarothanken@gmail.com>
2015-02-26 08:58:57 +0900

    Freezable関連をコメントアウト。要確認。 依存プロパティーの規定値を指定。 ストアアプリには、MessageBoxImage相当がない。影響範囲が読めなかったため、MessageBoxImageを取り込む。

--- Universal(8.1)/Livet(Shared)/Messaging/InformationMessage.cs
+++ Universal(8.1)/Livet(Shared)/Messaging/InformationMessage.cs
@@ -1,4 +1,4 @@
-﻿using System.Windows;
+﻿using Windows.UI.Xaml;
 
 namespace Livet.Messaging
 {
@@ -40,6 +40,8 @@ namespace Livet.Messaging
             Image = image;
         }
 
+        // TODO 親が Freezable クラスからの派生ではなくなったため、削除。……大丈夫？
+        /*
         /// <summary>
         /// 派生クラスでは必ずオーバーライドしてください。Freezableオブジェクトとして必要な実装です。<br/>
         /// 通常このメソッドは、自身の新しいインスタンスを返すように実装します。
@@ -49,6 +51,7 @@ namespace Livet.Messaging
         {
             return new InformationMessage(Text,Caption,MessageKey);
         }
+         */
 
         /// <summary>
         /// 表示するメッセージを指定、または取得します。
@@ -88,9 +91,52 @@ namespace Livet.Messaging
 
         // Using a DependencyProperty as the backing store for Image.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty ImageProperty =
-            DependencyProperty.Register("Image", typeof(MessageBoxImage), typeof(InformationMessage), new PropertyMetadata());
+            DependencyProperty.Register("Image", typeof(MessageBoxImage), typeof(InformationMessage), 
+                new PropertyMetadata(MessageBoxImage.Information));
 
-        
-        
+        /// <summary>
+        /// メッセージ ボックスで表示されるアイコンを指定します。
+        /// </summary>
+        /// <remarks>
+        /// System.Windows.MessageBoxImage 相当 (ユニバーサルアプリでは無効)
+        /// </remarks>
+        public enum MessageBoxImage
+        {
+            // 概要:
+            //     アイコンは表示されません。
+            None = 0,
+            //
+            // 概要:
+            //     メッセージ ボックスに、背景が赤の円で囲まれた白い X から成る記号を表示します。
+            Error = 16,
+            //
+            // 概要:
+            //     メッセージ ボックスに、背景が赤の円で囲まれた白い X から成る記号を表示します。
+            Hand = 16,
+            //
+            // 概要:
+            //     メッセージ ボックスに、背景が赤の円で囲まれた白い X から成る記号を表示します。
+            Stop = 16,
+            //
+            // 概要:
+            //     メッセージ ボックスに、円で囲まれた疑問符から成る記号を表示します。
+            Question = 32,
+            //
+            // 概要:
+            //     メッセージ ボックスに、背景が黄色の三角形で囲まれた感嘆符から成る記号を表示します。
+            Exclamation = 48,
+            //
+            // 概要:
+            //     メッセージ ボックスに、背景が黄色の三角形で囲まれた感嘆符から成る記号を表示します。
+            Warning = 48,
+            //
+            // 概要:
+            //     メッセージ ボックスに、円で囲まれた小文字の i から成る記号を表示します。
+            Information = 64,
+            //
+            // 概要:
+            //     メッセージ ボックスに、円で囲まれた小文字の i から成る記号を表示します。
+            Asterisk = 64,
+        }
     }
 }
