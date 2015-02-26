cb75847f9c0ccc763b43051eab12c299a946846c
YAMAMI Taro <mitarothanken@gmail.com>
2015-02-26 13:04:10 +0900

    対象をFrameworkElementに変更。 Focus()に対し、フォーカスの変更自由を指定する必要があったため、Programmatic を指定。

--- Universal(8.1)/Livet(Shared)/Behaviors/SetFocusAction.cs
+++ Universal(8.1)/Livet(Shared)/Behaviors/SetFocusAction.cs
@@ -1,16 +1,16 @@
 ﻿using System.Windows;
-using Windows.UI.Xaml;
 
 namespace Livet.Behaviors
 {
     /// <summary>
     /// アタッチしているコントロールにフォーカスを試みます。
     /// </summary>
-    public class SetFocusAction : TriggerAction<FrameworkElement>
+    /// TODO FrameworkElement から Control に変更したが……大丈夫？
+    public class SetFocusAction : TriggerAction<Windows.UI.Xaml.Controls.Control>
     {
         protected override void Invoke(object parameter)
         {
-            AssociatedObject.Focus();
+            AssociatedObject.Focus(Windows.UI.Xaml.FocusState.Programmatic);
         }
     }
 }
